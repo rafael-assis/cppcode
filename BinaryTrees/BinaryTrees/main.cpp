8c509c1f30bfb48d9b09da210e410ab110e7b8d3
Rafael Assis <rafael@rafael-mac.PK5001Z>
2014-11-03 00:52:57 -0800

    Adding first project: Binary Trees

---
+++ BinaryTrees/BinaryTrees/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BinaryTrees
+//
+//  Created by Rafael Assis on 11/3/14.
+//  Copyright (c) 2014 Rafael Assis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "This will be an implementation of a BST\n";
+    return 0;
+}
